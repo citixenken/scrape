93fca6e2e12b942583d5bd4527bccf2cc250a316
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-06 12:33:09 +0300

    application_controller.rb CRUD configurations

--- scrape-backend/app/controllers/application_controller.rb
+++ scrape-backend/app/controllers/application_controller.rb
@@ -7,4 +7,39 @@ class ApplicationController < Sinatra::Base
     articles.to_json
   end
 
+  #POST
+  post '/articles' do
+    article = Article.create(
+      title: params[:title],
+      web_url: params[:web_url],
+      byline: params[:byline],
+      author: params[:author],
+      date_published: params[:date_published]
+    )
+    article.to_json
+  end
+
+  #PATCH
+  patch '/articles/:id' do
+    article = Article.find(params[:id])
+    article.update(
+      title: params[:title],
+      web_url: params[:web_url],
+      byline: params[:byline],
+      author: params[:author],
+      date_published: params[:date_published]
+    )
+    article.to_json
+  end
+
+  #DELETE
+  delete '/articles/:id' do 
+    article = Article.find(params[:id])
+
+    article.destroy
+
+    article.to_json
+  end
+
+
 end
