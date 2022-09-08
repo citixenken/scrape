b2b722a072913702f0fc3c5b43f2d2a36bcfe173
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-08 06:10:13 +0300

    DELETE functionality: delete articles

--- scrape-frontend/src/components/articleitem/ArticleItem.js
+++ scrape-frontend/src/components/articleitem/ArticleItem.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const ArticleItem = ({ article }) => {
+const ArticleItem = ({ article, onDeleteArticle }) => {
   const { id, title, web_url, byline, author, date_published } = article;
+  const [favState, setFavState] = useState("");
+
+  //DELETE FROM DB
+  //==============
+  function handleArticleDelete() {
+    fetch(`http://localhost:9292/articles/${article.id}`, { method: "DELETE" })
+      .then((res) => res.json())
+      .then(() => onDeleteArticle(article))
+      .catch((err) => console.error(err));
+  }
+
+  //FAVOURITE ARTICLE ITEM
+  //===================
+  function handleFavState() {
+    setFavState((favState) => !favState);
+  }
+
   return (
     <div>
       <a href={web_url} target="_blank">
@@ -13,6 +31,21 @@ const ArticleItem = ({ article }) => {
         Written by: {author} on {date_published}
       </p>
       <br />
+      <button
+        className="ui submit primary button left floated"
+        onClick={handleFavState}
+      >
+        {favState ? "❤️Favorite" : "🤍Favorite"}
+      </button>
+
+      {/* DELETE BOOK */}
+      <button
+        onClick={handleArticleDelete}
+        className="ui submit basic red button right floated"
+        style={{ color: "red" }}
+      >
+        Delete
+      </button>
     </div>
   );
 };
--- scrape-frontend/src/components/articles/Articles.js
+++ scrape-frontend/src/components/articles/Articles.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import ArticleList from "../articleslist/ArticlesList";
+import ArticleItem from "../articleitem/ArticleItem";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [articleSearch, setArticleSearch] = useState("");
 
   const URL = "http://localhost:9292/articles";
 
@@ -13,9 +14,80 @@ const Articles = () => {
       .catch((err) => console.error(err));
   }, []);
 
+  //SEARCH ARTICLES
+  //===============
+  const articlesToDisplay = articles.filter((article) =>
+    article.title.toLowerCase().includes(articleSearch.toLowerCase())
+  );
+
+  //DELETE ARTICLE FROM DB
+  //======================
+  function handleArticleDelete(deletedArticle) {
+    const updatedArticleList = articles.filter(
+      (article) => article.id !== deletedArticle.id
+    );
+    setArticles(updatedArticleList);
+  }
+
   return (
     <div>
-      <ArticleList articles={articles} />
+      {/* <ArticleList articles={articles} /> */}
+      <div className="ui container">
+        <div className="ui very padded piled tertiary segment">
+          <h1
+            className="ui header huge"
+            style={{
+              color: "firebrick",
+              fontSize: 36,
+              fontFamily: "Varela Round",
+            }}
+          >
+            My Library Collection
+          </h1>
+        </div>
+
+        {/* SEARCH FIELD */}
+        <div className="ui very padded tertiary segment">
+          {/* SEARCH INPUT */}
+          <div className="ui input huge left icon fluid search focus ">
+            <input
+              type="text"
+              name="articleSearch"
+              value={articleSearch}
+              placeholder="Search Article By Title"
+              onChange={(e) => setArticleSearch(e.target.value)}
+            />
+            <i className="book icon"></i>
+          </div>
+        </div>
+        {/* SEARCH FIELD */}
+
+        <div className="ui very padded teal secondary inverted  segment">
+          {articles ? (
+            <div className="ui doubling very padded stackable grid container">
+              {articlesToDisplay.map((article, idx) => (
+                <div className="four wide column">
+                  <ArticleItem
+                    key={idx}
+                    article={article}
+                    onDeleteArticle={handleArticleDelete}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="ui massive very padded inverted segment">
+              <div className="ui active inverted dimmer">
+                <div className="ui indeterminate large text loader">
+                  <h6>Search To Retrieve Articles</h6>
+                </div>
+              </div>
+              <p></p>
+              <p></p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
