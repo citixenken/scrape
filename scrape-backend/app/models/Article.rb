7c277109954ed61c5ff76db449870808e4bed0ac
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-12 05:22:49 +0300

    updating models

--- scrape-backend/app/models/Article.rb
+++ scrape-backend/app/models/Article.rb
@@ -1,4 +1,16 @@
 class Article < ActiveRecord::Base
     has_many :reviews
     has_many :readers, through: :reviews
+
+    def leave_review(reader, rating, comment)
+        Review.create(comment: comment, rating: rating, reader_id: reader.id, article_id: self.id)
+    end
+
+    def print_all_reviews
+        reviews.filter { |review| review.articles == self }.map { |review| review.print_preview }
+    end
+
+    def average_rating
+        self.reviews.map { |review| review.rating }.reduce(:+).to_f / self.reviews.count
+    end
 end
--- scrape-backend/app/models/Review.rb
+++ scrape-backend/app/models/Review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
     belongs_to :article
     belongs_to :reader
+
+    def print_preview
+        "Review for #{article.title} by #{reader.username}: #{self.rating}. #{self.comment}"
+    end
 end
